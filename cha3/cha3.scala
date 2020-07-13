863321aead487ba2ba70a46922b27c0cba01d39b
Peizhi Zheng <pzheng16@asu.edu>
2020-07-13 11:15:21 -0700

    spark marchine learning

--- cha3/cha3.scala
+++ cha3/cha3.scala
@@ -13,3 +13,18 @@ staticDataFrame.selectExpr("customerID", "(UnitPrice * Quantity) as total_cost",
 val streamingDataFrame = spark.readStream.schema(staticSchema).option("maxFilesPerTrigger", 1).format("csv").option("header","true").load("./data/retail-data/by-day/*.csv")
 
 // streamingDataFrame.isStreaming
+
+val purchaseByCustomerPerHour = streamingDataFrame.selectExpr("customerID", "(UnitPrice * Quantity) as total_cost", "InvoiceDate").groupBy(
+    $"CustomerID", window($"InvoiceDate","1 day")).sum("total_cost")
+
+purchaseByCustomerPerHour.writeStream.format("memory").queryName("customer_purchases").outputMode("complete").start()
+
+
+//Machine Learning and Advanced Analytics
+staticDataFrame.printSchema()
+import org.apache.spark.sql.functions.date_format
+val preppedDataFrame = staticDataFrame.na.fill(0).withColumn("day_of_week", date_format($"InvoiceDate", "EEEE")).coalesce(5)
+
+//split the data into training and test sets
+val trainDataFrame = preppedDataFrame.where("InvoiceDate < '2011-07-01' ")
+val testDataFrame = preppedDataFrame.where("InvoiceData >= '2011-07-01' ")
